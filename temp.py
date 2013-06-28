2c184bd85fbf2ffd55a276a5bb8bb15e9a22774b
Technoboy10 <cchudson98@gmail.com>
2013-06-28 16:48:58 -0600

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,4 +1,14 @@
-if 'command' in path:
-    	regex = re.compile("\/command(othercommands)(in|regex)")
-		m = regex.match(path)
-		#do commands
+if path == '/return': #return data
+		f = open(ospath + '/return', 'w+')
+		f.write("your data")
+		f.close()
+		f = open(ospath + '/return', 'rb')
+		ctype = self.guess_type(ospath + '/return')
+		self.send_response(200)
+	        self.send_header("Content-type", ctype)
+	        fs = os.fstat(f.fileno())
+	        self.send_header("Content-Length", str(fs[6]))
+	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+	        self.send_header("Access-Control-Allow-Origin", "*")
+	        self.end_headers()
+	        return f
