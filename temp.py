56087a36c2f8535c56bd757711afb0476e609893
Technoboy10 <cchudson98@gmail.com>
2013-07-23 14:08:48 -0600

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,14 +1,22 @@
-if path == '/return': #return data
-		f = open(ospath + '/return', 'w+')
-		f.write(str(your data))
-		f.close()
-		f = open(ospath + '/return', 'rb')
-		ctype = self.guess_type(ospath + '/return')
-		self.send_response(200)
-	        self.send_header("Content-type", ctype)
-	        fs = os.fstat(f.fileno())
-	        self.send_header("Content-Length", str(fs[6]))
-	        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-	        self.send_header("Access-Control-Allow-Origin", "*")
-	        self.end_headers()
-	        return f
+if __name__ == "__main__":
+    print "WiiSnap by Technoboy10"
+    import sys
+    import re
+    import os
+    import SocketServer
+    import cwiid
+    ospath = os.path.abspath('')
+    print "Please connect a Wiimote by pressing 1 and 2 on your Wiimote."
+    #try:
+    wm = cwiid.Wiimote()
+    wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC | cwiid.RPT_MOTIONPLUS | cwiid.RPT_NUNCHUK
+    PORT = 1280 #Major device code for a Wiimote
+
+    Handler = CORSHTTPRequestHandler
+    #Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+    httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+    print "serving at port", PORT
+    print "Go ahead and launch Snap!."
+    httpd.serve_forever()
