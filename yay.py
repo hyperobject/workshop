7d7ec62bbc94105a418ff7310c1f6f4808a1fc5c
Technoboy10 <cchudson98@gmail.com>
2013-07-23 13:45:20 -0600

    Party woooo

--- yay.py
+++ yay.py
@@ -1,15 +1,8 @@
-elif 'getled' in path:
-	led = wm.state['led']
-	f = open(ospath + '/return', 'w+')
-	f.write(str(led))
-	f.close()
-	f = open(ospath + '/return', 'rb')
-	ctype = self.guess_type(ospath + '/return')
-	self.send_response(200)
-        self.send_header("Content-type", ctype)
-        fs = os.fstat(f.fileno())
-        self.send_header("Content-Length", str(fs[6]))
-        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-        self.send_header("Access-Control-Allow-Origin", "*")
-        self.end_headers()
-	return f
+elif 'illuminate' in path:
+	regex = re.compile('\/illuminate(on|off)')
+	m = regex.match(path)
+	setl = m.group(1)
+	if setl == 'on':
+		l.set_illuminated(True)
+	elif setl == 'off':
+		l.set_illuminated(False)
