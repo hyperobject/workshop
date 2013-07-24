9fa964f097c0eaa0c5dd0898e585a44d3715afd0
Technoboy10 <cchudson98@gmail.com>
2013-07-24 03:10:15 -0600

    I'm the Doctor

--- yay.py
+++ yay.py
@@ -1,44 +1,7 @@
-if __name__ == "__main__":
-    print "Snap-NXT by Technoboy10"
-    import re
-    import os
-    import SocketServer
-    import nxt.bluesock
-    import nxt.locator
-    from nxt.motor import *
-    from nxt.sensor import *
-    try:
-        b = nxt.locator.find_one_brick()
-    except:
-        print "NXT brick not found. Please connect an NXT brick and try again."
-        quit()
-    try:
-      t = Touch(b, PORT_1)
-    except:
-    	pass
-    try:
-    	s = Sound(b, PORT_2)
-    except:
-    	pass
-    try:
-    	l = Light(b, PORT_3)
-    except:
-    	pass
-    try:
-    	u = Ultrasonic(b, PORT_4)
-    except:
-    	pass
-    m_a = Motor(b, PORT_A)
-    m_b = Motor(b, PORT_B)
-    m_c = Motor(b, PORT_C)
-    PORT = 1330 #N reminded me of 13, X in Roman numerals is 10, and T is the 20th letter. I added X and T together.
-
-    Handler = CORSHTTPRequestHandler
-    #Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
-    httpd = SocketServer.TCPServer(("", PORT), Handler)
-
-    print "serving at port", PORT
-    print "Go ahead and launch Snap!."
-    print "<a>http://snap.berkeley.edu/snapsource/snap.html#open:http://localhost:1330/Snap-NXT</a>"
-    httpd.serve_forever()
+import os
+import re
+import SocketServer
+import pyttsx
+import urllib2
+PORT = 59591 #T+T+S twice
+engine = pyttsx.init()
